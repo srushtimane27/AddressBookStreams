b4cae2734bc75951fabda2b7ae401ebc4f6deeff
srushtimane27 <srushtimane31@gmail.com>
2022-08-02 15:18:17 +0530

    Abilty to ensure there is duplicate entry of same person

--- src/main/java/AddressBookStreams.java
+++ src/main/java/AddressBookStreams.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class AddressBookStreams {
-}
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AddressBookStreams {
+
+    public static void main(String[] args) {
+        List<String> StringListFirstName = Arrays.asList("Srushti","Srushti","Ajay","Rasika","Ruchika","Jidnya");
+        List<String> StringListLastName = Arrays.asList("Mane","Mane","Jadhav","Pawar","Shinde","Kadam");
+
+        Set<String> result = findDuplicateBySetAdd(StringListFirstName);
+        Set<String> result1 = findDuplicateBySetAdd(StringListLastName);
+        result.forEach(System.out::println);
+        result1.forEach(System.out::println);
+    }
+    public static <T> Set<T>findDuplicateBySetAdd(List<T> list){
+        Set<T> items = new HashSet<>();
+        return list.stream()
+                .filter(n -> !items.add(n))
+                .collect(Collectors.toSet());
+    }
+    }
+
+
+
---
+++ src/main/java/JavaStreamExample.java
@@ -0,0 +1,56 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.Comparator;
+
+
+class Person{
+    int id;
+    String firstname;
+    String lastname;
+    String address;
+    String city;
+    String state;
+    int zip;
+    int phonenumber;
+    String mail;
+    public Person(int id, String firstname, String lastname, String address, String city, String state, String zip, String phonenumber, String mail) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = Integer.parseInt(zip);
+        this.phonenumber = Integer.parseInt(phonenumber);
+        this.mail = mail;
+    }
+}
+public class JavaStreamExample {
+    public static void main(String[] args) {
+        List<Person> personsList = new ArrayList<Person>();
+
+        //Add Persons
+        personsList.add(new Person(2,"Srushti", "Mane","Shahupuri", "Satara", "Maharashtra","12457","45786125","abc@gmail.com"));
+        personsList.add(new Person(1,"Rasika","Mule","Chinchwad","Pune","Maharashtra","56478","15236478","bhdg@gmail.com"));
+        personsList.add(new Person(3,"Ruchika","Jadhav","Dapodi","Vasai","Gujrat","561424","214578965","huif@gmail.com"));
+
+
+        //UC8:Search person in city or state
+        personsList.stream()
+                .filter(person -> person.city == "Pune")
+                .forEach(person -> System.out.println(person.firstname));
+
+        //UC9:View person by city or state
+        personsList.stream()
+                .filter(person -> person.city == "Satara")
+                .forEach(person -> System.out.println(person.phonenumber));
+
+        //UC10:Get Number by city or state
+        personsList.stream()
+                .filter(person -> person.state == "Gujrat")
+                .forEach(person -> System.out.println(person.phonenumber));
+
+
+    }
+}
